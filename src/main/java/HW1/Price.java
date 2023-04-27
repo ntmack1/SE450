2222bc7dd4ca7edcca1dfb34bf00218c9ce3b532
ntmack1 <ntmack5@gmail.com>
2023-04-26 23:14:30 -0500

    Created PriceFactory and Main

--- src/main/java/HW1/Price.java
+++ src/main/java/HW1/Price.java
@@ -1,5 +1,7 @@
 package HW1;
 
+import java.util.Objects;
+
 public class Price {
 
     int price;
@@ -8,52 +10,59 @@ public class Price {
         this.price = price;
     }
     boolean isNegative() {
-        return this.price < 0 ? true : false;
+        return this.price < 0;
     }
-//    Price add(Price p) {
-//
-//    }
-//
-//    Price subtract(Price p) {
-//
-//    }
-//
-//    Price multiply(int n) {
-//
-//    }
-//
-//    boolean greaterOrEqual(Price p) {
-//
-//    }
-//
-//    boolean lessOrEqual(Price p) {
-//
-//    }
-//
-//    boolean greaterThan(Price p) {
-//
-//
-//    }
-//
-//    boolean lessThan(Price p) {
-//
-//    }
-//
-//    boolean equals(Object o) {
-//
-//    }
-//
-//    int compareTo(Price p) {
-//
-//    }
-//
-//    String toString() {
-//
-//    }
-//
-//    int hashCode() {
-//
-//    }
+    Price add(Price p) {
+        return new Price(this.price + p.price);
+    }
+
+    Price subtract(Price p) {
+        return new Price(this.price - p.price);
+    }
+
+    Price multiply(int n) {
+        return new Price(this.price * n);
+    }
+
+    boolean greaterOrEqual(Price p) {
+        return this.price >= p.price;
+    }
+
+    boolean lessOrEqual(Price p) {
+        return this.price <= p.price;
+    }
+
+    boolean greaterThan(Price p) {
+        return this.price > p.price;
+    }
+
+    boolean lessThan(Price p) {
+        return this.price < p.price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return price == price1.price;
+    }
+
+    int compareTo(Price p) {
+        return this.price - p.price;
+    }
+
+    @Override
+    public String toString() {
+        return "Price{" +
+                "price=" + price +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
 
 }
 
