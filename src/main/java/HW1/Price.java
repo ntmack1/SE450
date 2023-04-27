0912107d4425811ead7a977b4022b502c6c412b1
ntmack1 <ntmack5@gmail.com>
2023-04-26 19:17:06 -0500

    Created Price Class

--- src/main/java/HW1/Price.java
+++ src/main/java/HW1/Price.java
@@ -1,4 +1,52 @@
 package HW1;
 
 public class Price {
+    boolean isNegative() {
+
+    }
+    Price add(Price p) {
+
+    }
+
+    Price subtract(Price p) {
+
+    }
+
+    Price multiply(int n) {
+
+    }
+
+    boolean greaterOrEqual(Price p) {
+
+    }
+
+    boolean lessOrEqual(Price p) {
+
+    }
+
+    boolean greaterThan(Price p) {
+
+
+    }
+
+    boolean lessThan(Price p) {
+
+    }
+
+    boolean equals(Object o) {
+
+    }
+
+    int compareTo(Price p) {
+
+    }
+
+    String toString() {
+
+    }
+
+    int hashCode() {
+
+    }
+
 }
